1707c9bcd9c71e11e8036a9c44d03643a5c7220d
Stephen Pennell <stepenn7@gmail.com>
2016-05-16 18:00:32 -0400

    Removes RequireJS
    * Uses Browserify to bundle the app
    * Adds notification on save

--- bin/stashdown.js
+++ bin/stashdown.js
@@ -1,52 +1,38 @@
 #!/usr/bin/env node
 var requirejs = require('requirejs');
 var path = require('path');
+var program = require('commander');
+var server = require('../lib/server');
 
-requirejs.config({
-  baseUrl: __dirname,
-  config: {
-    '../lib/server': {
-      dir: path.resolve(__dirname, '../lib/')
+program
+  .version('v1.0.0');
+
+program
+  .command('new', null, {isDefault: true})
+  .description('Create a Markdown file from scratch')
+  .action(function(){
+    server.start();
+  });
+
+program
+  .command('edit [file]')
+  .description('Edit a Markdown file')
+  .action(function(file){
+    if (!file) {
+      console.log('No file supplied!');
     }
-  }
-});
+    server.start(file);
+  });
 
-requirejs([
-  'commander',
-  '../lib/server'
-], function(program, server) {
+program
+  .command('stop')
+  .description('Shutdown a running server instance')
+  .action(function(){
+    server.stop();
+  });
 
-  program
-    .version('v1.0.0');
+program.parse(process.argv);
 
-  program
-    .command('new')
-    .description('Create a Markdown file from scratch')
-    .action(function(){
-      server.start();
-    });
-
-  program
-    .command('edit [file]')
-    .description('Edit a Markdown file')
-    .action(function(file){
-      if (!file) {
-        console.log('No file supplied!');
-      }
-      server.start(file);
-    });
-
-  program
-    .command('stop')
-    .description('Shutdown a running server instance')
-    .action(function(){
-      server.stop();
-    });
-
-  program.parse(process.argv);
-
-  if (program.args.length === 0) {
-    program.help();
-  }
-
-});
+if (program.args.length === 0) {
+  server.start();
+}
